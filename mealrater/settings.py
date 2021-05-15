7b9f87724ed9d08226a6391fe04cba632709a4a3
Fahd Mannaa <fahdmannaa@macs-Mac-mini.home>
2021-05-15 09:12:39 +0300

    4_token_auth

--- mealrater/settings.py
+++ mealrater/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -70,6 +71,11 @@ DATABASES = {
     }
 }
 
+# REST_FRAMEWORK = {
+#             'DEFAULT_PERMISSION_CLASSES': {
+#                 'rest_framework.permissions.IsAuthenticated',
+#             }
+#         }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ video4.txt
@@ -0,0 +1,10 @@
+1- URL to request token of specific user 
+    Token 
+
+2- Only authenticated users can create\update meal rate
+
+3- Register users
+    URL > ViewSet > Serializer > ser model = User default django 
+    URL > ViewSet > Serializer > ser model = User custom abstract
+
+4- general security checks 
